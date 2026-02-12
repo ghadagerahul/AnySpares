d17a9adf3d8dab8912b0683af956c305206718be
Ghadage Rahul <inb322rahgha@ext.crifnet.com>
2026-02-12 23:53:26 +0530

    DDD commit-2

---
+++ Anyspare-TrackingService/src/main/java/com/trackingservice/app/AnyspareTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trackingservice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyspareTrackingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyspareTrackingServiceApplication.class, args);
+	}
+
+}
